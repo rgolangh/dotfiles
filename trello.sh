999f218a854dc0aa266d901e02d907b875f67437
Roy Golan <rgolan@redhat.com>
2018-04-29 17:52:53 +0300

    add trello command
    Signed-off-by: Roy Golan <rgolan@redhat.com>

---
+++ trello.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# To make this work I added an alias under ~/.mailrc
+# with 'alias trello theaddress@ofmyboard.trello.com'
+# The mail should arrive into my default board's backlog list.
+function todo() {
+  echo "$@" | mail trello
+}
