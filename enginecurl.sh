dc4ff924340a1f66a94378d84a9b5b21638b5428
Roy Golan <rgolan@redhat.com>
2018-04-29 17:52:24 +0300

    parameterize some of the aliases
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- enginecurl.sh
+++ enginecurl.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+export ENGINE_URL=http://localhost:28080
+
 function enginecurl () 
 { 
     local uri=$1;
     shift;
-    curl -s -u admin@internal:123 -H "All-Content: true" -H "Accept: application/json" $@ http://localhost:8080/ovirt-engine/api/$uri
+    curl -s -u admin@internal:123 -H "All-Content: true" -H "Accept: application/json" $@ ${ENGINE_URL}/ovirt-engine/api/$uri
 }
