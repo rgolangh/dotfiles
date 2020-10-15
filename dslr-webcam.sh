540c5492c7f6af5435e8f04246729e4372e30f93
Roy Golan <rgolan@redhat.com>
2020-10-15 09:34:51 +0300

    Add errexit to dslr-webcam
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- dslr-webcam.sh
+++ dslr-webcam.sh
@@ -1,11 +1,10 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 
 device=${1-/dev/video2}
-
 echo "Using device $device"
 
+# Create a virtual video device using this kernel module
 sudo modprobe v4l2loopback exclusive_caps=1 max_buffers=2
-
 echo "Loaded kernel module"
 
 gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 $device
