2352440f348bf316338164228f633c613fc0aad4
Roy Golan <rgolan@redhat.com>
2020-10-15 09:39:52 +0300

    Add virtual video device script (v4l2loopback)
    Signed-off-by: Roy Golan <rgolan@redhat.com>

---
+++ virtual-video-dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -ex
+
+# if the module fails to load it most probably means
+# it needs to be built and install to the current kernel
+modinfo v4l2loopback || installModule
+
+# Create a virtual video device using this kernel module
+sudo modprobe v4l2loopback exclusive_caps=1 max_buffers=2
+echo "Loaded kernel module"
+
+function installModule() {
+	cd $HOME/src/v4l2loopback
+	make clean
+	make
+	sudo make install
+}
