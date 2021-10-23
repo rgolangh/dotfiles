f94d8447f12d6b5adcf4c1ff670d7c648163f420
Roy Golan <rgolan@redhat.com>
2021-10-23 08:46:01 +0300

    Formatting for dslr-cam
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- dslr-cam.sh
+++ dslr-cam.sh
@@ -3,8 +3,6 @@
 # This scripts enables a a toggle on/off of a virtual camera. Combining with dslr-cam.desktop
 # launcher makes it even more convenient.
 
-
-
 function app() {
 	# this makes sure we kill all child process when the app exists
 	trap 'kill $(jobs -p)' EXIT
@@ -16,7 +14,6 @@ function app() {
 
 	gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 "$device" &> /dev/null
 }
-
 
 source $(dirname $(readlink -f ${BASH_SOURCE[0]}))/toggle-helper.sh
 toggle app
