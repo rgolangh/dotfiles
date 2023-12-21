107c05c5c2fcf10655f85c1b8c65bfe404a7bfba
Roy Golan <rgolan@redhat.com>
2023-12-21 17:04:10 +0200

    lint
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- productivity.sh
+++ productivity.sh
@@ -2,20 +2,20 @@
 
 
 function dnd() {
-	v=$(gsettings get org.gnome.desktop.notifications show-banners)
-	gsettings set org.gnome.desktop.notifications show-banners $([ "$v" == "true" ] && echo false || echo true)
+    v=$(gsettings get org.gnome.desktop.notifications show-banners)
+    gsettings set org.gnome.desktop.notifications show-banners $([ "$v" == "true" ] && echo false || echo true)
 }
 
 
 function noise() {
-	pid=/var/run/user/$UID/noise.pid
-	if [ -f $pid ];then
-		kill $(cat $pid)
-		rm $pid
-		return
-	fi
-	nohup play -n synth brownnoise synth brownnoise mix synth sine amod 0 &
-	echo $! > $pid
+    pid=/var/run/user/$UID/noise.pid
+    if [ -f $pid ];then
+        kill $(cat $pid)
+        rm $pid
+        return
+    fi
+    nohup play -n synth brownnoise synth brownnoise mix synth sine amod 0 &
+    echo $! > $pid
 }
 
 function remove_unknown_ssh() {
