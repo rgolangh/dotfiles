d088e23542b9ffd3465899bb835f37e60ceee142
Roy Golan <rgolan@redhat.com>
2024-01-04 19:15:55 +0200

    ns: no need to complete after the first word
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- kubectl.sh
+++ kubectl.sh
@@ -13,6 +13,10 @@ function ns() {
 }
 
 _ns_completions() {
+    # no need to try to complete if we already have a namespace - e.g 'ns somenamespace <tab> <tab>'
+    if [ "${#COMP_WORDS[@]}" != "2" ]; then
+        return
+    fi
     COMPREPLY=($(compgen -W "$(oc get ns -o jsonpath="{range .items[*]}{.metadata.name }{' '}")" -- "${COMP_WORDS[1]}"))
 #    for word in $(oc get ns -o jsonpath="{range .items[*]}{.metadata.name }{' '}"); do
 #        COMPREPLY+=("${word}")
