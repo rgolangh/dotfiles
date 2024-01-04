c2e2f334c3222ca9a71881f1cc0ebd5519c88df0
Roy Golan <rgolan@redhat.com>
2024-01-04 19:17:29 +0200

    ns: Add comment about the article
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- kubectl.sh
+++ kubectl.sh
@@ -12,6 +12,7 @@ function ns() {
     kubectl config set-context --current --namespace "${1:-default}"
 }
 
+# inspired and learned a lot from this article - https://opensource.com/article/18/3/creating-bash-completion-script
 _ns_completions() {
     # no need to try to complete if we already have a namespace - e.g 'ns somenamespace <tab> <tab>'
     if [ "${#COMP_WORDS[@]}" != "2" ]; then
