4c8775300f5553135a9ff970be501635fe7a0a50
Roy Golan <rgolan@redhat.com>
2024-02-26 15:20:05 +0200

    updates
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- kubectl.sh
+++ kubectl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export KUBECONFIG="/var/home/rgolan/.kube/config"
-KUBECONFIG+=":/var/home/rgolan/clusters/hub-helios04/ocp-dev-cluster/my-cluster/auth/kubeconfig"
-KUBECONFIG+=":/var/home/rgolan/clusters/kind/kubeconfig"
+export KUBECONFIG="${HOME}/.kube/config"
+KUBECONFIG+=":${HOME}/clusters/hub-helios04/ocp-dev-cluster/my-cluster/auth/kubeconfig"
+KUBECONFIG+=":${HOME}/clusters/kind/kubeconfig"
 
 source <(kubectl completion bash)
 alias k='kubectl'
--- rust-script.rs
+++ rust-script.rs
@@ -1,10 +1,19 @@
 #!/usr/bin/env -S /home/rgolan/.cargo/bin/cargo -Zscript
 //Excercise in rust (while using new cargo single file package feature)
 fn main() {
+    foo();
     println!("Printing a-z A-Z as \"A\" | \"B\" ...  (Backus–Naur form)");
     for (i, c) in  ('A'..'Z').chain('a'..'z').enumerate() {
         let new_line = if (i+1) % 10 == 0  { "\n" } else {""};
         print!("\"{}\" |{new_line}", c);
     }
 
+    let a: i8 = 4;
+    print!(a);
+}
+
+// foo those this 
+// a: an int
+fn foo(a: i8) {
+    print!(a)
 }
